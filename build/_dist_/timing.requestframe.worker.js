38f8f15315684586f9108bc1d02042e081c86f8c
designerzen <designerzen@gmail.com>
2020-07-16 13:59:17 +0100

    Switched from yarn to npm for github actions

---
+++ build/_dist_/timing.requestframe.worker.js
@@ -0,0 +1,78 @@
+const CMD_START = "start";
+const CMD_STOP = "stop";
+const CMD_UPDATE = "update";
+const EVENT_READY = "ready";
+const EVENT_STARTING = "starting";
+const EVENT_STOPPING = "stopping";
+const EVENT_TICK = "tick";
+let isRunning = false;
+let startTime = -1;
+let currentTime = -1;
+let nextInterval = -1;
+let gap = 0;
+let intervals = 0;
+let exit = false;
+const elapsed = () => (performance.now() - startTime) * 0.001;
+const reset = () => {
+  intervals = 0;
+  startTime = performance.now();
+};
+const loop = () => {
+  if (exit) {
+    exit = false;
+    return;
+  }
+  currentTime = performance.now();
+  if (currentTime >= nextInterval) {
+    nextInterval += gap;
+    intervals++;
+    postMessage({
+      event: EVENT_TICK,
+      time: elapsed(),
+      intervals
+    });
+  }
+  requestAnimationFrame(loop);
+};
+const start = (interval = 250) => {
+  gap = interval;
+  startTime = performance.now();
+  nextInterval = startTime + interval;
+  if (!isRunning) {
+    isRunning = true;
+    postMessage({
+      event: EVENT_STARTING,
+      time: 0,
+      intervals
+    });
+  }
+  loop();
+  postMessage({
+    event: EVENT_TICK,
+    time: elapsed(),
+    intervals
+  });
+};
+const stop = () => {
+  exit = true;
+  isRunning = false;
+  postMessage({
+    event: EVENT_STOPPING,
+    time: elapsed(),
+    intervals
+  });
+};
+self.onmessage = (e) => {
+  const data = e.data;
+  switch (data.command) {
+    case CMD_START:
+      start(data.interval);
+      break;
+    case CMD_STOP:
+      stop();
+      break;
+    case CMD_UPDATE:
+      start(data.interval);
+      break;
+  }
+};
