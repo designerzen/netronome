6e202fe98456cb2df5fdaf89f2a8ed5fa29ba287
zen <designerzen@gmail.com>
2026-04-14 13:40:20 +0100

    fix: flattened output where possible

--- src/index.ts
+++ src/index.ts
@@ -17,20 +17,20 @@
  */
 
 // Main Timer class and types
-export { default as Timer } from './src/timer.ts'
-export type { ITimerControl, TimingHandler, TimerCallbackEvent } from './src/timer-interfaces.ts'
+export { default as Timer } from './timer.ts'
+export type { ITimerControl, TimingHandler, TimerCallbackEvent } from './timer-interfaces.ts'
 
 // Timer configuration and options
-export * from './src/timer-types.ts'
-export * from './src/timer-options.ts'
-export * from './src/timer-event-types.ts'
-export * from './src/timer-interfaces.ts'
+export * from './timer-types.ts'
+export * from './timer-options.ts'
+export * from './timer-event-types.ts'
+export * from './timer-interfaces.ts'
 
 // Event types and worker types
-export * from './src/timer-worker-types.ts'
+export * from './timer-worker-types.ts'
 
 // Time utilities for calculations
-export * from './src/time-utils.ts'
+export * from './time-utils.ts'
 
 // Global timer functions (singleton pattern)
 export { 
@@ -40,10 +40,10 @@ export {
   setTimeBetween, 
   resetTimer, 
   getTimer 
-} from './src/timer-global.ts'
+} from './timer-global.ts'
 
 // AudioContext-based timer
-export { default as AudioTimer } from './src/timer-audio.ts'
+export { default as AudioTimer } from './timer-audio.ts'
 
 // Tap tempo detection
-export { tapTempoQuick, tapTempo } from './src/tap-tempo.ts'
+export { tapTempoQuick, tapTempo } from './tap-tempo.ts'
