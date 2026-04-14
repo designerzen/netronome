79ebf951afda2b2bf5fd11934027da45ad9a7ed7
zen <designerzen@gmail.com>
2026-04-14 13:41:07 +0100

    fix: flattened output to dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -15,15 +15,15 @@
  * })
  * ```
  */
-export { default as Timer } from './src/timer.ts';
-export type { ITimerControl, TimingHandler, TimerCallbackEvent } from './src/timer-interfaces.ts';
-export * from './src/timer-types.ts';
-export * from './src/timer-options.ts';
-export * from './src/timer-event-types.ts';
-export * from './src/timer-interfaces.ts';
-export * from './src/timer-worker-types.ts';
-export * from './src/time-utils.ts';
-export { createTimer, startTimer, stopTimer, setTimeBetween, resetTimer, getTimer } from './src/timer-global.ts';
-export { default as AudioTimer } from './src/timer-audio.ts';
-export { tapTempoQuick, tapTempo } from './src/tap-tempo.ts';
+export { default as Timer } from './timer.ts';
+export type { ITimerControl, TimingHandler, TimerCallbackEvent } from './timer-interfaces.ts';
+export * from './timer-types.ts';
+export * from './timer-options.ts';
+export * from './timer-event-types.ts';
+export * from './timer-interfaces.ts';
+export * from './timer-worker-types.ts';
+export * from './time-utils.ts';
+export { createTimer, startTimer, stopTimer, setTimeBetween, resetTimer, getTimer } from './timer-global.ts';
+export { default as AudioTimer } from './timer-audio.ts';
+export { tapTempoQuick, tapTempo } from './tap-tempo.ts';
 //# sourceMappingURL=index.d.ts.map
--- dist/src/timer-worker-types.d.ts
+++
@@ -1,6 +0,0 @@
-export { default as AudioContextWorkerWrapper } from './workers/timing.audiocontext.worker.ts?worker';
-export { default as RollingTimeWorkerWrapper } from './workers/timing.rolling.worker.ts?worker';
-export { default as SetIntervalWorkerWrapper } from './workers/timing.setinterval.worker.ts?worker';
-export { default as SetTimeoutWorkerWrapper } from './workers/timing.settimeout.worker.ts?worker';
-export { TIMER_TYPE_AUDIO_CONTEXT, TIMER_TYPE_AUDIO_WORKLET, TIMER_TYPE_ROLLING, TIMER_TYPE_SET_INTERVAL, TIMER_TYPE_SET_TIMEOUT, TIMER_TYPES, isValidTimerType, getTimerTypeDescription, type TimerType, } from './timer-types';
-//# sourceMappingURL=timer-worker-types.d.ts.map
